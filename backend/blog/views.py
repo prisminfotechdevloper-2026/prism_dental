1d67f010b57966b2580481f56cdc888e7801b1a0
pintusingh123 <pintugit982@gmail.com>
2026-09-15 20:46:54 +0530

    done completely

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -4,7 +4,9 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
+# pyrefly: ignore [missing-import]
 from .models import BlogPost
+# pyrefly: ignore [missing-import]
 from .serializers import BlogPostSerializer
 
 class BlogPostListCreateView(APIView):
